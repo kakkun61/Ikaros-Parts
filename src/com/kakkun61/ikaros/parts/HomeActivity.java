c669d84f37f9530b2df341c6cdf4be054941860c
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2014-04-13 14:26:30 +0900

    1st commit

---
+++ src/com/kakkun61/ikaros/parts/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.kakkun61.ikaros.parts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}

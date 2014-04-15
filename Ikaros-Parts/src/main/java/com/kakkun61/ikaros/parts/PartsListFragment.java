3382902f936c6ccefef80022f68e6ea1921e282c
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2014-04-15 21:38:42 +0900

    copy source files from master (commit 994b3e50f9e0c4525cce5138e3e22c4b505e5b42)

---
+++ Ikaros-Parts/src/main/java/com/kakkun61/ikaros/parts/PartsListFragment.java
@@ -0,0 +1,30 @@
+package com.kakkun61.ikaros.parts;
+
+import android.app.ListFragment;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
+import com.kakkun61.ikaros.parts.db.IkarosSQLiteOpenHelper;
+
+public class PartsListFragment extends ListFragment {
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final SQLiteDatabase db = new IkarosSQLiteOpenHelper(getActivity()).getReadableDatabase();
+        final Cursor items = db.query("Items", null, null, null, null, null, null, null);
+        setListAdapter(new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.pats_list_row,
+                items,
+                new String[]{"Rank", "ID"},
+                new int[]{R.id.rank, R.id.name},
+                0
+        ));
+    }
+}
+
+class RowData {
+
+}
